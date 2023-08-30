967499fa4993a769c06e4cccb03ffa46d83d70ee
Keila Freitas <keilafreitas767@gmail.com>
2023-08-29 23:26:37 -0300

    correção no código

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-document.getElementById("submitButton").addEventListener("click", function() {
-    var numA = parseFloat(document.getElementById("numA").value);
-    var numB = parseFloat(document.getElementById("numB").value);
-    var messageElement = document.getElementById("message");
+document.getElementById("submitButton").addEventListener("click", function () {
+	let numberA = parseFloat(document.getElementById("numberA").value);
+	let numberB = parseFloat(document.getElementById("numberB").value);
+	let messageElement = document.getElementById("message");
 
-    if (isNaN(numA) || isNaN(numB)) {
-     messageElement.innerHTML = "Por favor, insira números válidos em ambus os campos. ";
-     messageElement.style.color ="red";
-     
-    }else if (numB > numA) {
-     messageElement.innerHTML = "Formulário válido! Número B é maior que o número A." ;
-
-    }else {
-     messageElement.innerHTML = "Formulário inválido! Número B deve ser maior que o número A. "
-     messageElement.style.color = "red";
-    }
- });
+	if (isNaN(numberA) || isNaN(numberB)) {
+		messageElement.innerHTML =
+			"Por favor, insira números válidos em ambos os campos. ";
+		messageElement.style.color = "red";
+	} else if (numberB > numberA) {
+		messageElement.innerHTML =
+			"Formulário válido! Número B é maior que o número A.";
+            messageElement.style.color = "green";
+	} else {
+		messageElement.innerHTML =
+			"Formulário inválido! Número B deve ser maior que o número A. ";
+		messageElement.style.color = "red";
+	}
+});
